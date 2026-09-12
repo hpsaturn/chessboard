f7f06fb12cbbbac658df884f48d7c4cf1cf0a614
Hpsaturn <hpsaturn@gmail.com>
2026-09-12 23:56:14 +0200

    v0.1.6 undo and FEN improvements

--- src/version.cpp
+++ src/version.cpp
@@ -3,15 +3,15 @@
 
 // These will be replaced by CMake during build
 #ifndef PROJECT_VERSION
-#define PROJECT_VERSION "v0.1.3"
+#define PROJECT_VERSION "v0.1.6"
 #endif
 
 #ifndef GIT_VERSION
-#define GIT_VERSION "v0.1.3-unknown"
+#define GIT_VERSION "v0.1.6-unknown"
 #endif
 
 #ifndef BUILD_INFO
-#define BUILD_INFO "v0.1.3-unknown"
+#define BUILD_INFO "v0.1.6-unknown"
 #endif
 
 std::string Version::getVersion() {
